e73ea5618fe1886e5814f5a78ad65d735841d97f
Evan K <ekcopersonal@gmail.com>
2024-05-23 16:55:26 -0400

    fix: calling environmental variable dependency, identify bundle api schema bug

--- pipe/blocks_txs_bundles.py
+++ pipe/blocks_txs_bundles.py
@@ -3,12 +3,18 @@ import polars as pl
 import requests
 import time
 
-from lancedb_tables.lance_table import LanceTable
 
 import asyncio
 import hypersync
 
+from dotenv import load_dotenv
+from lancedb_tables.lance_table import LanceTable
+
+
 from hypersync import ColumnMapping, DataType, TransactionField, BlockField
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 def fetch_libmev_bundles(timestamp_start, timestamp_end, limit=100, offset=0, order_by="block_number"):
@@ -32,7 +38,7 @@ def fetch_libmev_bundles(timestamp_start, timestamp_end, limit=100, offset=0, or
     - Prefect will automatically retry the task up to 5 times with a 10-second delay between retries if the task ultimately fails.
     """
     # get endpoint from .env file
-    endpoint = os.getenv("LIBMEV_API_ENDPOINT")
+    endpoint = os.getenv("LIBMEV_ENDPOINT")
     params = {
         "timestampRange": f"{timestamp_start},{timestamp_end}",
         "filterByTags": ",".join(["naked_arb", "backrun", "sandwich", "liquidation"]),
@@ -60,6 +66,49 @@ def fetch_libmev_bundles(timestamp_start, timestamp_end, limit=100, offset=0, or
 
 def fetch_paginated_data(timestamp_start, timestamp_end, limit=100, pages=1000):
     """
+    ! TODO - there is currently a bug where the last iteration of data throws an error:
+    Traceback (most recent call last):
+  File "/home/evan/Documents/mev_pipe/pipe/blocks_txs_bundles.py", line 220, in <module>
+    asyncio.run(historical_blocks_txs_sync())
+  File "/home/evan/.rye/py/cpython@3.12.2/install/lib/python3.12/asyncio/runners.py", line 194, in run
+    return runner.run(main)
+           ^^^^^^^^^^^^^^^^
+  File "/home/evan/.rye/py/cpython@3.12.2/install/lib/python3.12/asyncio/runners.py", line 118, in run
+    return self._loop.run_until_complete(task)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/home/evan/.rye/py/cpython@3.12.2/install/lib/python3.12/asyncio/base_events.py", line 685, in run_until_complete
+    return future.result()
+           ^^^^^^^^^^^^^^^
+  File "/home/evan/Documents/mev_pipe/pipe/blocks_txs_bundles.py", line 214, in historical_blocks_txs_sync
+    lance_tables.write_table(uri="data", table="libmev_bundles", data=bundle_df, merge_on="block_number"
+  File "/home/evan/Documents/mev_pipe/.venv/lib/python3.12/site-packages/lancedb_tables/lance_table.py", line 59, in write_table
+    .execute(data)
+     ^^^^^^^^^^^^^
+  File "/home/evan/Documents/mev_pipe/.venv/lib/python3.12/site-packages/lancedb/merge.py", line 107, in execute
+    self._table._do_merge(self, new_data, on_bad_vectors, fill_value)
+  File "/home/evan/Documents/mev_pipe/.venv/lib/python3.12/site-packages/lancedb/table.py", line 1644, in _do_merge
+    new_data = _sanitize_data(
+               ^^^^^^^^^^^^^^^
+  File "/home/evan/Documents/mev_pipe/.venv/lib/python3.12/site-packages/lancedb/table.py", line 125, in _sanitize_data
+    data = _sanitize_schema(
+           ^^^^^^^^^^^^^^^^^
+  File "/home/evan/Documents/mev_pipe/.venv/lib/python3.12/site-packages/lancedb/table.py", line 1766, in _sanitize_schema
+    return pa.Table.from_arrays(
+           ^^^^^^^^^^^^^^^^^^^^^
+  File "pyarrow/table.pxi", line 3974, in pyarrow.lib.Table.from_arrays
+  File "pyarrow/table.pxi", line 1464, in pyarrow.lib._sanitize_arrays
+  File "pyarrow/array.pxi", line 371, in pyarrow.lib.asarray
+  File "pyarrow/table.pxi", line 566, in pyarrow.lib.ChunkedArray.cast
+  File "/home/evan/Documents/mev_pipe/.venv/lib/python3.12/site-packages/pyarrow/compute.py", line 404, in cast
+    return call_function("cast", [arr], options, memory_pool)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "pyarrow/_compute.pyx", line 590, in pyarrow._compute.call_function
+  File "pyarrow/_compute.pyx", line 385, in pyarrow._compute.Function.call
+  File "pyarrow/error.pxi", line 154, in pyarrow.lib.pyarrow_internal_check_status
+  File "pyarrow/error.pxi", line 91, in pyarrow.lib.check_status
+pyarrow.lib.ArrowTypeError: struct fields don't match or are in the wrong order: 
+
+
     Fetch paginated data from the libmev API by calling fetch_libmev_bundles repeatedly.
 
     Parameters:
@@ -74,21 +123,21 @@ def fetch_paginated_data(timestamp_start, timestamp_end, limit=100, pages=1000):
     """
     all_data = []
     total_rows = 0
+
     for page in range(pages):
         offset = page * limit
         data = fetch_libmev_bundles(
             timestamp_start, timestamp_end, limit=limit, offset=offset)
-        if data and "data" in data:
-            fetched_data = data["data"]
-            if len(fetched_data) == 0:
-                print(f"Page {page + 1}: Fetched 0 rows, exiting the loop.")
-                break  # Stop if 0 rows are fetched
-            all_data.extend(fetched_data)
-            total_rows += len(fetched_data)
-            print(f"Page {page + 1}: Fetched {len(fetched_data)
-                                              } rows, Total: {total_rows} rows")
-        else:
-            break  # Stop if there's no more data to fetch
+
+        fetched_data = data["data"]
+        if len(fetched_data) == 0:
+            print(f"Page {page + 1}: Fetched 0 rows, exiting the loop.")
+            break  # Stop if 0 rows are fetched
+        all_data.extend(fetched_data)
+        total_rows += len(fetched_data)
+        print(f"Page {page + 1}: Fetched {len(fetched_data)
+                                          } rows, Total: {total_rows} rows")
+
     return all_data
 
 
@@ -109,7 +158,8 @@ async def historical_blocks_txs_sync():
     # set to_block and from_block to query the desired block range.
     block_height: int = await client.get_height()
     to_block = block_height - 64  # 2 epochs past
-    from_block: int = to_block - (7200 * 2)  # sample
+    # 8 days, because the 8th day crashes due to an error with libmev non-schema standardization
+    from_block: int = to_block - (7200 * 8)
 
     # add +/-1 to the block range because the query is exclusive to the block number
     query = client.preset_query_blocks_and_transactions(
@@ -118,7 +168,7 @@ async def historical_blocks_txs_sync():
     query.max_num_transactions = 1_000  # for troubleshooting
 
     # batch size for processing the hypersync query and writing table to lancedb
-    db_batch_size: int = 7_200
+    db_batch_size: int = 7200
 
     while from_block < to_block:
         current_to_block = min(from_block + db_batch_size, to_block)
